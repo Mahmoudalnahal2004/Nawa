df27b4c9c614255ba80524a7ce82d7d0d6399587
mahmoud alnahal <mahmoudalnahal47@gmail.com>
2026-05-28 19:57:17 +0300

    fix cors

--- apps/api/app/main.py
+++ apps/api/app/main.py
@@ -75,7 +75,11 @@ app = FastAPI(
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.cors_origins_list,
+    allow_origins=[
+    "https://nawa-blond.vercel.app",
+    "https://nawa-kjtjbbbi8-mahmoudalnahal2004-projects.vercel.app",
+    "http://localhost:3000",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
